30a48a6842c1188193169b98121ed9c164328535
CelesteTang <optimism2025@gmail.com>
2017-05-08 02:30:21 +0800

    Add reusable view

---
+++ DemoForTableViewAndCollectionView/DemoFooterCollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  DemoFooterCollectionReusableView.swift
+//  DemoForTableViewAndCollectionView
+//
+//  Created by 湯芯瑜 on 2017/5/8.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+import UIKit
+
+class DemoFooterCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
---
+++ DemoForTableViewAndCollectionView/DemoHeaderCollectionReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  DemoHeaderCollectionReusableView.swift
+//  DemoForTableViewAndCollectionView
+//
+//  Created by 湯芯瑜 on 2017/5/8.
+//  Copyright © 2017年 Hsin-Yu Tang. All rights reserved.
+//
+
+import UIKit
+
+class DemoHeaderCollectionReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
