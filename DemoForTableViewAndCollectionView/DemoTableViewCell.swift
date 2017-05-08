16a423b14db4cc0046b839b582614a4bdcbb2200
CelesteTang <optimism2025@gmail.com>
2017-05-09 00:27:53 +0800

    Add screenshots

--- DemoForTableViewAndCollectionView/DemoTableViewCell.swift
+++ DemoForTableViewAndCollectionView/DemoTableViewCell.swift
@@ -20,11 +20,5 @@ class DemoTableViewCell: UITableViewCell {
         demoLabel.numberOfLines = 0
 
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
